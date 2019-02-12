e5fe71ef33ba93373b75edbf14ae104ee062a644
AlexKarnei <46269001+AlexKarnei@users.noreply.github.com>
2019-02-12 03:55:59 +0300

    second commit

--- src/main/java/by/itstep/karnei/MyArrayList.java
+++ src/main/java/by/itstep/karnei/MyArrayList.java
@@ -1,52 +1,175 @@
 package by.itstep.karnei;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.Iterator;
+
+public class MyArrayList<E> implements MyList<E> {
+    private E[] data;
+    private static int capacity = 10;
+    private static int size = 0;
 
 
-class MyArrayList<T> {
-
-    private T[] data;
-    private static int CAPACITY = 10;
-    private int size;
-    public int capacity;
-
-    public MyArrayList(int capacity) {
-        if (capacity > 0) {
-            data = (T[]) new Object[capacity];
-        } else if (capacity == 0) {
-            data = (T[]) new Object[0];
-        }else {
+    public MyArrayList(int insureCapacity) {
+        if (insureCapacity >= 0) {
+            this.data = (E[]) new Object[insureCapacity];
+        } else {
             throw new ExceptionInInitializerError();
         }
     }
 
     public MyArrayList() {
-        MyArrayList data = new MyArrayList(10);
+        this.data = (E[]) new Object[capacity];
     }
 
     public int getSize() {
         return size;
     }
 
-    public String toString() {
-        String stringData = "";
-        for (int i = 0; i < data.length; i++) {
-            stringData += data[i];
-            stringData += " ";
-        }
-        return stringData;
+    private void rangeCheck(int index) {
+        if (index >= size)
+            throw new IndexOutOfBoundsException("Index is not valid");
     }
 
-    private void ensureCapacity(int capacity) {
-        this.capacity = capacity;
-        if (capacity > CAPACITY) {
-            while (data.length > CAPACITY) {
-                CAPACITY = (int) (CAPACITY * 1.5 + 1);
-                this.data = (T[]) new Object[CAPACITY];
+    private void resize(int newLength) {
+        E[] temp = data;
+        data = (E[]) new Object[newLength];
+        System.arraycopy(temp, 0, data, 0, size);
+    }
+
+    public String toString() {
+        String string = "";
+        for (int i = 0; i < data.length; i++) {
+            string += data[i];
+            string += " ";
+        }
+        return string;
+    }
+
+    private void ensureCapasity(int capasity) {
+
+    }
+
+
+    public boolean pushBack(E element) {
+        try {
+            if (size > data.length - 1) {
+                resize((int) (capacity * 1.5) + 1);
+                data[size++] = element;
+            } else {
+                data[size++] = element;
+            }
+            return true;
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean popFront() {
+        try {
+            E[] temp = data;
+            data = (E[]) new Object[temp.length];
+            size--;
+            System.arraycopy(temp, 1, data, 0, temp.length - 1);
+            return true;
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean pushFront(E element) {
+        try {
+            if (size + 1 > data.length) {
+                E[] temp = data;
+                data = (E[]) new Object[temp.length + 1];
+                System.arraycopy(temp, 0, data, 1, temp.length);
+                data[0] = element;
+                size++;
+            } else {
+                E[] temp = data;
+                data = (E[]) new Object[temp.length];
+                System.arraycopy(temp, 0, data, 1, temp.length - 1);
+                data[0] = element;
+                size++;
+            }
+            return true;
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean insert(E element, int index) {
+        rangeCheck(index);
+        try {
+            E[] temp = data;
+            if (index + 1 <= data.length && size + 1 <= data.length) {
+                if (index > size) {
+                    size = index + 1;
+                } else {
+                    size++;
+                }
+                data = (E[]) new Object[temp.length];
+                System.arraycopy(temp, 0, data, 0, index);
+                data[index] = element;
+                int otherElement = temp.length - index - 1;
+                System.arraycopy(temp, index, data, index + 1, otherElement);
+                return true;
+            } else {
+                throw new ArrayIndexOutOfBoundsException("Out of bounds array");
+            }
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean removeAt(int index) {
+        rangeCheck(index);
+        E[] temp = data;
+        data = (E[]) new Object[temp.length];
+        System.arraycopy(temp, 0, data, 0, index);
+        int otherElement = data.length - index - 1;
+        size--;
+        if (otherElement > 0)
+            System.arraycopy(temp, index + 1, data, index, otherElement);
+
+        return true;
+    }
+
+    public boolean remove(E element) {
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(element)) {
+                removeAt(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removeAll(E element) {
+        for (int i = 0; i < data.length; i++) {
+            if (element.equals(data[i])) {
+                data[i] = null;
+                size--;
             }
         }
     }
 
+    public void popBack() {
+        removeAt(size - 1);
+    }
 
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new MyIterator<>(data);
+    }
 }
